ee2415b0c2c611fda5fb971aee4e5bb944e230ba
Tim Sullivan <timothyj.sullivan1@gmail.com>
2023-08-29 13:37:51 -0500

    added azure ad app settings & testing new program.cs

--- Demo.FileUploadWebApp/Client/Program.cs
+++ Demo.FileUploadWebApp/Client/Program.cs
@@ -9,19 +9,15 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo.FileUploadWebApp.Client
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
-    }
-
-}
+await builder.Build().RunAsync();
