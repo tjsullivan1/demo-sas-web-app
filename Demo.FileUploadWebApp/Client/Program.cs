af66a06cc05af7f6ce57e4b5f34bdc2e77e80818
Tim Sullivan <timothyj.sullivan1@gmail.com>
2023-08-29 13:58:36 -0500

    commented out failing component

--- Demo.FileUploadWebApp/Client/Program.cs
+++ Demo.FileUploadWebApp/Client/Program.cs
@@ -17,7 +17,7 @@ namespace Demo.FileUploadWebApp.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+        //  builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
